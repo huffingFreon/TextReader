444026ce9759f55c84710ca15e0b5c88f73f474b
huffingFreon <higginsba@etsu.edu>
2019-09-12 13:41:31 -0400

    Created word and sentence classes

--- TextReader/TextReader/TextReader.cs
+++ TextReader/TextReader/TextReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TextReader
@@ -8,7 +9,16 @@ namespace TextReader
     {
         public static void Main()
         {
+            string path = @"e:\Classes\CSCI 2910\Lab 2\TheMythOfSisyphus.txt";
 
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
