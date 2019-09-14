18d3a0f0d6675980ce863ea116fdaff7cd581a39
huffingFreon <higginsba@etsu.edu>
2019-09-13 22:56:08 -0400

    Started Words class

--- TextReader/TextReader/TextReader.cs
+++ TextReader/TextReader/TextReader.cs
@@ -8,12 +8,20 @@ namespace TextReader
         public static void Main()
         {
             string strPath = @"e:\Classes\CSCI 2910\Lab 2\TheMythOfSisyphus.txt";
+            FileInfo file = new FileInfo(strPath);
 
             string text = File.ReadAllText(strPath);
             string moreText = "Here,.,.,,,..,. is. a, sentence. with. too. much. punctuation.";
             string[] symbols = { ".", "," };
 
-            Console.WriteLine(Sentence.RemoveChars(moreText, symbols));
+            using(StreamReader sr = file.OpenText())
+            {
+                var s = "";
+                while((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
---
+++ TextReader/TextReader/Words.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TextReader
+{
+    public class Words
+    {
+        public List<string> wordList = new List<string>(); 
+
+        public Words(List<string> existingList)
+        {
+            foreach(var v in existingList)
+            {
+                wordList.Add(v);
+            }
+        }
+
+        public Words(FileInfo file)
+        {
+            string strText = "";
+            string[] words;
+
+            using(StreamReader sr = file.OpenText())
+            {
+                var s = "";
+                while((s = sr.ReadLine()) != null)
+                {
+                    strText += s;
+                }
+            }
+
+            words = strText.Split(' ');
+
+            foreach(string word in words)
+            {
+                wordList.Add(word);
+            }
+        }
+
+        public Words(string allText)
+        {
+            string[] moreWords = allText.Split(' ');
+
+            foreach(string word in moreWords)
+            {
+                wordList.Add(word);
+            }
+        }
+
+        //Use Regex.Matches() within this class
+        public Dictionary<string, int> Occurrence()
+        {
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            return occurrences;
+        }
+
+        //Use Regex.Matches() here too if it works
+        public Dictionary<char, int> CharOccurrence()
+        {
+            Dictionary<char, int> charOccurrences = new Dictionary<char, int>();
+
+            return charOccurrences;
+        }
+    }
+}
